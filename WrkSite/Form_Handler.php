b812bffae608ff7ad9c4a5205fb4e40061baf592
Jarell <jarellml@gmail.com>
2023-07-19 01:00:22 -0400

    new cmd

---
+++ WrkSite/Form_Handler.php
@@ -0,0 +1,22 @@
+<DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=utf-8" />
+<style type="text/css" title="text/css" media="all">
+.error {
+	font-weight: bold;
+	color: #C00;
+}
+</style>
+<title> Form Feedback</title>
+</head>
+
+<body>
+<?php # Script 2.2 - Form_Handler.php
+#FirstName = $_REQUEST['FirstName'];
+#LastName = $_REQUEST['LastName']
+#Password = $_REQUEST['Password']
+#Email = $_REQUEST['Email']
+#UserName = $_REQUEST['UserName']
+?>
+</body>
